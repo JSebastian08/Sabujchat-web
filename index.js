1ed0a4926de7be00d146b47ee70f90944ed72723
amSebas <sebaslgr@gmail.com>
2023-06-15 00:56:27 -0500

    se agregan detalles finales de la product page

---
+++ index.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector("#modal")
+const abrirModal = document.querySelector("#modal-btn")
+const abrirModal2 = document.querySelector("#modal-btn-2")
+const abrirModal3 = document.querySelector("#modal-btn-3")
+const abrirModal4 = document.querySelector("#modal-btn-4")
+const cerrarModal = document.querySelector("#close-modal")
+
+abrirModal.addEventListener("click", ()=>{
+    modal.showModal();
+})
+
+abrirModal2.addEventListener("click", ()=>{
+    modal.showModal();
+})
+
+abrirModal3.addEventListener("click", ()=>{
+    modal.showModal();
+})
+
+abrirModal4.addEventListener("click", ()=>{
+    modal.showModal();
+})
+
+cerrarModal.addEventListener("click", ()=>{
+    modal.close();
+})
+
